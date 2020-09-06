176ad75d5cc6e18c0ead8daae002af512d0a0de1
Carlos Pérez <hvzrzr08@gmail.com>
2020-09-05 20:40:55 -0700

    App referente a todo list en react

---
+++ app1b/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+//importing components
+import Form from './components/Form'
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+      <header>
+      <div>
+          hellow shane...
+          I'm really gussbones
+          I'm put on a headeir
+      </div>
+      <Form />
+      <TodoList/>
+      </header>
+  );
+}
+
+export default App;
