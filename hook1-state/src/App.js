bafe72c5a6f08d1bad0917946211ac4c5ee8eec3
Carlos <hvzrzr08@gmail.com>
2020-09-15 20:27:37 -0700

    mismo contador ahora con hook useState

--- hook1-state/src/App.js
+++ hook1-state/src/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
+import React, { useState }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-    //def de state contiene var contador con edo. 0
-state = {
-    contador: 0,
-}
+function App () {
+            //edo.     fx(f5-edo)
+    const [contador, setContador] = useState(0)
 
-// metodo que actualizará la var de contador
-incrementar = () => {
-    this.setState({contador: this.state.contador + 1})
-}
-  render() {
-
-      //sacar la var contador para poder renderizar
-       const {contador} = this.state 
+    const incrementar = () => {
+        setContador(contador + 1);
+    }
     return (
     <div className="App">
       <header className="App-header">
@@ -23,11 +16,9 @@ incrementar = () => {
         <p>
             {contador}
         </p>
-          <button onClick={this.incrementar}>Incrementar</button>
+          <button onClick={incrementar}>Incrementar</button>
       </header>
     </div>
         );
-    }
 }
-  
 export default App;
